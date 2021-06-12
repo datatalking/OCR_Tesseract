f60d68349c1d0a52d63de13daff261dfd6611393
Andrew <paintstone@gmail.com>
2021-06-12 06:43:28 -0700

    update LSTM with function vs global

--- LSTM_RNN_410.py
+++ LSTM_RNN_410.py
@@ -20,6 +20,25 @@ from matplotlib import pyplot
 #  according to a user-given fitness, error or reward function
 # TODO msg Jurgen Schmidthuber for colab
 
+
+
+def main():
+
+def bounding_box():
+	from pytesseract import Output
+	d = pytesseract.image_to_data(img, output_type=Output.DICT)
+	n_boxes = len(d['level'])
+	for i in range(n_boxes):
+		if (d['text'][i] != ""):
+			(x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+			cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	
+	cv2.imwrite('result.png', img)
+
+
+
+
+
 args = {"image":"../input/text-detection/example-images/Example-images/ex24.jpg",
         "east":"../input/text-detection/east_text_detection.pb",
         "min_confidence":0.5, "width":320, "height":320}
@@ -35,30 +54,47 @@ default arguments
 :param: INPUTPATH as literal location for future training data
 :param: OUTPUTPATH as literal location for where files are marked, organized and or user input is needed
 :param:
-
 :return 
 """
 
-# give location of the image to be read.
-# "Example-images/ex24.jpg"
 
-args['image']="../input/text-detection/example-images/Example-images/ex24.jpg"
-image = cv2.imread(args['image'])
+def image_to_string():
+	img_cv = cv2.imread(r'/<path_to_image>/digits.png')
+	
+	# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
+	# we need to convert from BGR to RGB format/mode:
+	img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+	print(pytesseract.image_to_string(img_rgb))
+	# OR
+	img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
+	print(pytesseract.image_to_string(img_rgb))
 
-# saving training data originals with shape vs recursive issues
-orig = image.copy()
-(origH, origW) = image.shape[:2]
 
-# set new height and width to default 320 by using args dictionary
-(args["width"], args["height"])
+def resize_file():
+	# give location of the image to be read.
+	# "Example-images/ex24.jpg"
+	
+	args['image'] = "../input/text-detection/example-images/Example-images/ex24.jpg"
+	image = cv2.imread(args['image'])
+	
+	# saving training data originals with shape vs recursive issues
+	orig = image.copy()
+	(origH, origW) = image.shape[:2]
+	
+	# set new height and width to default 320 by using args dictionary
+	(args["width"], args["height"])
+	
+	# ratio between original and new image for both height and weight.
+	(newW, NewH) = (args["width"], args["height"])
+	
+	# Calculate the rstio between original and new image  for both height and weight
+	# This ratio will be used to translate buding box location on the orinal images
+	rW = origW / float(newW)
+	rH = origH / float(newH)
+	
+	# resize the originl image to new dimensions
+	image = cv2 =
 
-# ratio between original and new image for both height and weight.
-(newW, NewH) = (args["width"], args["height"])
 
-# Calculate the rstio between original and new image  for both height and weight
-# This ratio will be used to translate buding box location on the orinal images
-rW = origW / float(newW)
-rH = origH / float(newH)
-
-# resize the originl image to new dimensions
-image = cv2 =
+if __name__ == '__main__':
+	main()
