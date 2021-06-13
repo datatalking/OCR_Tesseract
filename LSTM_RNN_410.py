c0823245e792d16789eb44317b5ba02fd8a99a7c
Andrew Schell <11813209+datatalking@users.noreply.github.com>
2021-06-13 10:24:57 -0700

    updated for template

--- LSTM_RNN_410.py
+++ LSTM_RNN_410.py
@@ -11,6 +11,8 @@
 
 import numpy as np
 import cv2
+!pip install opencv-python # NOTE: *not* opencv-contrib-python
+pip install easyocr
 from imutils.object_detection import non_max_suppression
 import pytesseract
 from matplotlib import pyplot
